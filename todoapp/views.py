bd64603bbc9d952c3d6878a871e9a2da8ad94492
yukichi0412 <ykt.0412@gmail.com>
2021-09-13 23:25:33 +0900

    Make Index page

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Category
 
 # Create your views here.
+def index(request, id):
+    categories = Category.objects.filter(created_at__lte=timezone.now()).order_by('created_at')
+    return render(request, 'todoapp/index.html', {'categories':categories})
+
