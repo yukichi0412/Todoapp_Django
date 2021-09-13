181b0c98b112afa06c7042a6f91e9a16c80af6a3
yukichi0412 <ykt.0412@gmail.com>
2021-09-13 20:07:54 +0900

    make CategoryModel

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,3 +1,5 @@
+from todoapp.models import Category
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+admin.site.register(Category)
--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
