74c3f1c3e46a2ac4a707e43d581636fb631bc425
yukichi0412 <ykt.0412@gmail.com>
2021-09-14 18:33:46 +0900

    Added TaskModel

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
-from .models import Category
+from .models import Category, Task
 
 # Create your views here.
 def index(request, id):
     categories = Category.objects.filter(created_at__lte=timezone.now()).order_by('created_at')
-    return render(request, 'todoapp/index.html', {'categories':categories})
+    current_category = get_object_or_404(Category, id=id)
+    tasks = Task.objects.filter(category_id = current_category.id)
+    
+    return render(request, 'todoapp/index.html', 
+        {'categories':categories, 'tasks':tasks, 'current_category_id':current_category.id})
 
