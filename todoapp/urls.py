ddddf77cbeb63568e0365c68ef77df88c3390390
yukichi0412 <ykt.0412@gmail.com>
2021-09-17 00:55:28 +0900

    Add EditTask view, url, and Template

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('<int:id>/tasks', views.index, name='tasks.index'),
     path('create', views.create_category, name='categories.create'),
     path('<int:id>/tasks/create', views.create_task, name='tasks.create'),
+    path('<int:id>/tasks/<int:task_id>', views.edit_task, name='tasks.edit'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -37,3 +37,15 @@ def create_task(request, id):
     else:
         form = TaskForm()
     return render(request, 'todoapp/create_task.html', {'form': form}, {'id': current_category.id})
+
+def edit_task(request, id, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect('tasks.index', id=task.category_id.id)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todoapp/edit.html', {'form': form}, {'task': task})
